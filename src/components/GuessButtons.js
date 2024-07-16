1f661749f9eae5ee7a358d6d419c6de6742c2268
Steven Sawtelle <3885841+StevenSawtelle@users.noreply.github.com>
2024-07-16 00:20:03 -0700

    small changes

--- src/components/GuessButtons.js
+++ src/components/GuessButtons.js
@@ -7,7 +7,7 @@ const GuessButtons = ({ onGuess }) => (
     <div className={'guess-buttons'}>
         <button className={'male'} onClick={() => onGuess('male')}>Male</button>
         <button className={'female'} onClick={() => onGuess('female')}>Female</button>
-        <a data-tooltip-id="my-tooltip" data-tooltip-content="All authors currently available are strictly Male/Female!">
+        <a data-tooltip-id="my-tooltip" data-tooltip-content="All authors currently available are Male/Female!">
             <button className={'neither'} disabled title="Not available yet">Neither</button>
         </a>
         <Tooltip id="my-tooltip"/>
